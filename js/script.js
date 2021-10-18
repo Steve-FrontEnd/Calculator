ee98a4387f2d28a797ce06038babac40c50db030
stephen.chege@student.moringaschool.com <stephen.chege@student.moringaschool.com>
2021-10-18 10:22:52 +0300

    Your branch is ahead of 'origin/main' by 2 commits.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,39 @@
-var add = function(number1, number2) {
-    return number + number2;
-    };
-    var number1 = parseInt(prompt("Enter a number:"));
-    var number2 = parseInt(prompt("Enter another number:"));
-    alert(add(number1, number2));
-    
+function getDay(name){
+    var month = parseInt(document.getElementById("month").value);
+    var date = parseInt(document.getElementById("date").value);
+    var year = document.getElementById("year").value;
+    var century = year.slice(0, 1);
+    var years = year.slice(2, 3);
+
+    if (month === "" || month < 1 || month > 12 || date === "" || date < 0 || date > 31 || year === "" || year.length > 4 || name === ""){
+        alert("Confirm that you have entered the correct information!")
+    }else{
+        var day = Math.floor((((century/4)-2*century-1)+((5*years/4))+((26*(month+1)/10)) + date) % 7);
+        return day
+    }
+}
+function getGender(){
+    var radio = document.getElementsByName("gender");  
+
+    for (var i = 0; i < radio.length; i++){
+        if (radio[i].checked){
+            var gender = radio[i].value;
+            return gender;
+        }
+    }
+}
+
+function getName(){
+    var name =  document.getElementById("name").value;
+
+    var male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+    var female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
+    if (getGender() === "male"){
+        document.getElementById("post").innerHTML=(`Hey ${name}! if you were born in Ghana your name would be ${male[getDay(name)]}`);
+    }else if(getGender() === "female"){
+        document.getElementById("post").innerHTML=(`Hey ${name}! if you were born in Ghana your name would be ${female[getDay(name)]}`);
+    }else{
+        alert("Confirm that you have selected your gender")
+    }
+}
