273196dc6013589e1c658507da2997426499cc6b
stephen.chege@student.moringaschool.com <stephen.chege@student.moringaschool.com>
2021-10-19 07:48:50 +0300

    Your branch is up to date with 'origin/main'.

--- js/script.js
+++ js/script.js
@@ -1,39 +1,46 @@
-function getDay(name){
-    var month = parseInt(document.getElementById("month").value);
-    var date = parseInt(document.getElementById("date").value);
-    var year = document.getElementById("year").value;
-    var century = year.slice(0, 1);
-    var years = year.slice(2, 3);
-
-    if (month === "" || month < 1 || month > 12 || date === "" || date < 0 || date > 31 || year === "" || year.length > 4 || name === ""){
-        alert("Confirm that you have entered the correct information!")
-    }else{
-        var day = Math.floor((((century/4)-2*century-1)+((5*years/4))+((26*(month+1)/10)) + date) % 7);
-        return day
-    }
-}
-function getGender(){
-    var radio = document.getElementsByName("gender");  
-
-    for (var i = 0; i < radio.length; i++){
-        if (radio[i].checked){
-            var gender = radio[i].value;
-            return gender;
+let btn = document.getElementById('submitBtn');
+btn.addEventListener('click', () =>{    
+    validateDate()
+    // clear the input fields
+    document.getElementById('akanForm').reset();
+})
+// validation for date and gender
+const validateDate = () => {
+    let date = document.getElementById('date').value;
+    let radioBtns = document.querySelectorAll('input[name="gridRadios"]');
+    let selectedGender;
+    radioBtns.forEach(radiobtn => {
+        if(radiobtn.checked) {
+            selectedGender = radiobtn.value
         }
+    })    
+    let errorMsg = document.getElementById('error');
+    if (date === "" || selectedGender === undefined){
+        errorMsg.classList.add('error');
+    }else {
+        // console.log(`${date} ${selectedGender}`)
+        findAkanName(date, selectedGender);
+        errorMsg.classList.remove('error');
     }
 }
 
-function getName(){
-    var name =  document.getElementById("name").value;
-
-    var male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-    var female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-
-    if (getGender() === "male"){
-        document.getElementById("post").innerHTML=(`Hey ${name}! if you were born in Ghana your name would be ${male[getDay(name)]}`);
-    }else if(getGender() === "female"){
-        document.getElementById("post").innerHTML=(`Hey ${name}! if you were born in Ghana your name would be ${female[getDay(name)]}`);
-    }else{
-        alert("Confirm that you have selected your gender")
+// returns an Akan name
+const findAkanName = (date, gender) => {
+/* Day of the week (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7  */
+    let cc = date.split('-')[0].slice(0, 2);
+    let yy = date.split('-')[0].slice(2, date.split('-')[0].length);
+    let month = date.split('-')[1];
+    let day = date.split('-')[2];
+    let maleNames = [ 'Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
+    let femaleNames = [ 'Akosua', 'Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama']
+    let dayOfTheWeek = parseInt((((cc / 4) - 2 * cc - 1) + (5 * yy / 4) + (26 * (month + 1) / 10) + day) % 7)
+    if (gender == 'female'){
+        document.getElementById('akan').innerHTML = femaleNames[dayOfTheWeek];
+    }else {
+        document.getElementById('akan').innerHTML = maleNames[dayOfTheWeek];
     }
+  
 }
+
+
+
